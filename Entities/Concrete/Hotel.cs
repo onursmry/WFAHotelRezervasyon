26eaac57079df766b51f0fda572e90237da92007
onursmry <onursmry@hotmail.com>
2022-06-25 21:59:40 +0300

    xxtansiyon güncellemesi

--- Entities/Concrete/Hotel.cs
+++ Entities/Concrete/Hotel.cs
@@ -8,7 +8,7 @@ namespace WFAHotelRezervasyon.Entities.Concrete
     {
         public string HotelName { get; set; }
         public Locations HotelLocation { get; set; }
-        public AccomodationTypes AccomodationType { get; set; }
+        public AccommodationTypes AccommodationType { get; set; }
         public double Price { get; set; }
         public string Picture { get; set; }
 
--- Enums/AccommodationTypes.cs
+++ Enums/AccommodationTypes.cs
@@ -6,7 +6,7 @@ using System.Threading.Tasks;
 
 namespace WFAHotelRezervasyon.Enums
 {
-    public enum AccomodationTypes
+    public enum AccommodationTypes
     {
         Single,
         Double,
